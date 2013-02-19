6b43bdf5608926ebd3c55fb5dd6090d5cf63dc77
= <=>
2013-02-19 14:36:15 +0700

    first commit

---
+++ ilc/urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns #for static handler
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'portal.views.depan', name='depan'),
+    url(r'^daftar/$', 'portal.views.daftar', name="daftar"),
+    url(r'^jadwal/$', 'portal.views.Jadwal', name="jadwal"),
+    url(r'^lokasi/$', 'portal.views.Lokasi', name="lokasi"),
+    url(r'^gallery/$', 'portal.views.gallery', name="gallery"),    
+    url(r'^penginapan/$', 'portal.views.Penginapan', name="penginapan"),    
+    url(r'^faq/$', 'portal.views.FaQ', name="faq"),    
+    url(r'^sponsor/$', 'portal.views.Sponsor', name="sponsor"),    
+    url(r'^hubungi/$', 'portal.views.Hubungi', name="hubungi"),  
+    url(r'^informasi/$', 'portal.views.Informasi', name="informasi"),  
+    # url(r'^ilc/', include('ilc.foo.urls')), #for decloping URL
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^tamong123/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   		
+   )
---
+++ portal/views.py
@@ -0,0 +1,39 @@
+# Create your views here.
+from models import *
+from django.shortcuts import render_to_response
+from django.http import Http404
+
+def depan(request):
+	return render_to_response ('index.html', locals())
+	
+def daftar(request):
+	return render_to_response ('daftar.html', locals())	
+
+def Jadwal(request):
+	JAdwal = jadwal.objects.all()
+	return render_to_response ('jadwal.html', locals())		
+
+def Lokasi(request):
+	return render_to_response ('lokasi.html', locals())		
+	
+def gallery(request):
+	return render_to_response ('gallery.html', locals())				
+	
+def Penginapan(request):
+	PEnginapan = penginapan.objects.all()
+	return render_to_response ('penginapan.html', locals())			
+	
+def FaQ(request):
+	abc = faq.objects.all()
+	return render_to_response ('faq.html', locals())		
+
+def Sponsor(request):
+	SPonsor = sponsor_and_partner.objects.all()
+	return render_to_response ('sponsor.html', locals())													
+
+def Hubungi(request):
+	return render_to_response ('hubungi.html', locals())														
+	
+def Informasi(request):
+	INformasi = informasi.objects.all()
+	return render_to_response ('informasi.html', locals())															
